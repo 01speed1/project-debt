6aacbe809d831d95cd38bde1136b13177be98f10
Oscar Guzman <o1speed1@gmail.com>
2022-10-25 21:14:53 -0500

    get event name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    println!("Cachones app!");
+    println!("Ingresa el nombre del evento");
+
+    let mut event_name = String::new();
+
+    let terminal_input_reader = io::stdin();
+
+    terminal_input_reader.read_line(&mut event_name)?;
+
+    // create and instace of event
+   println!("nombre del evento {}", event_name);
+
+
+    Ok(())
 }
