b48ee17d1935934fe109a053a4e1d3c4713b09d4
Oscar Guzman <o1speed1@gmail.com>
2022-10-29 13:51:46 -0500

    add loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::io;
 
-fn main() -> io::Result<()> {
+struct Event {
+    name: String,
+    friends: Vec<Friend>
+}
+
+struct Friend {
+    name: String
+}
+
+fn main() {
     println!("Cachones app!");
     println!("Ingresa el nombre del evento");
 
@@ -8,11 +17,30 @@ fn main() -> io::Result<()> {
 
     let terminal_input_reader = io::stdin();
 
-    terminal_input_reader.read_line(&mut event_name)?;
+    terminal_input_reader.read_line(&mut event_name).unwrap();
+    
+    println!("Ingresa el nombre de los cachones que te acompañan al evento");
 
-    // create and instace of event
-   println!("nombre del evento {}", event_name);
+    let mut should_add_friend = true;
 
+    let mut friends = Vec::new();
 
-    Ok(())
+    while should_add_friend  {
+        let mut friend_name = String::new();
+        println!("Ingresa el nombre del cachon:");
+        terminal_input_reader.read_line(&mut friend_name).unwrap();
+        friends.push(friend_name);
+
+        println!("quieres agregar otro? (Y/N)");
+        let mut response = String::new();
+        terminal_input_reader.read_line(&mut response).unwrap();
+
+        match response {
+            "Y".to_string() => should_add_friend = false,
+            _ => should_add_friend = true
+        }
+    }
+        
+    let event = Event { name: event_name, friends: friends };
+
 }
